3644eb21c84efc7e1ca2efccd8cf4cd1940d0413
DreamType <anupamgak07@gmail.com>
2015-05-25 16:41:27 +0530

    Airtel website scraper added

---
+++ Airtel/address/address/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for address project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'address'
+
+SPIDER_MODULES = ['address.spiders']
+NEWSPIDER_MODULE = 'address.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'address (+http://www.yourdomain.com)'
